139c0b4f695a35220fe0c72dd9b481bbe584572b
Matt Kneiser <mattman@umich.edu>
2013-11-08 22:44:45 -0500

    Lot of client-side work

--- waferdb_client.js
+++ waferdb_client.js
@@ -1,14 +1,87 @@
 // Client-side API
 // Will expose functionality underneath a global 'wafer' object.
 var wafer = (function(){
-  var API = {};
+  var socket = io.connect('http://localhost:9000')
+    , API    = {}
+    , cache  = {}
+  ;
 
-  API.get = function(key){
-    console.log('client_get('+key+')');
+  function inCache(key){
+    // Lookup the key in the cache
+    // returns 'true' if the key is in the cache
+    // returns 'false' otherwise
+    return true; // for testing purposes only
+  }
+
+  function getKey(key){
+    // Retrieve the value associated with the given key from the cache
+    return true; // for testing purposes only
   };
 
-  API.put = function(key, value){
+  function writeCache(key, value){
+
+  }
+
+  /**
+    * GET:
+    *  responds from cache
+    *  otherwise goes to server
+    *
+    */
+  API.get = function(key, cb){
+    console.log('client_get('+key+')');
+
+    if(!inCache(key)) {
+      /**
+        * GET from server
+        *
+        */
+      socket.emit('get', { 'key': key });
+      socket.on('get_ack', function(data) {
+        console.log('#get_ack#');
+        console.log(data);
+
+        // return to user
+        cb({ 'result': data });
+      });
+    } else {
+      // Retrieve from cache & return to user
+      cb({ 'result': getKey(key) });
+    }
+  };
+
+  /**
+    * PUT:
+    *  writes value to server
+    *  writes value to cache
+    *
+    */
+  API.put = function(key, value, cb){
     console.log('client_put('+key+', '+value+')');
+
+    /**
+      * PUT to server
+      *
+      */
+    socket.emit('put', { 'key': key, 'value': value });
+    socket.on('put_ack', function(data) {
+      console.log('#put_ack#');
+      console.log(data);
+
+      if(data.result === 'error') {
+
+        // return to user
+        cb({ 'result': 'error' });
+      } else {
+        if(inCache(key)) {
+          // Modify cache after server's put_ack
+          writeCache(key, value);
+        }
+
+        // return to user
+        cb({ 'result': 'successful' });
+      }
+    });
   };
 
   return API;
--- waferdb_server.js
+++ waferdb_server.js
@@ -1,15 +1,38 @@
-var consistency_levels = ['refresh-on-dirty', 'flush-on-dirty', 'fire-and-forget']
+var io                 = require('socket.io').listen(9000)
+  , consistency_levels = ['refresh-on-dirty', 'flush-on-dirty', 'fire-and-forget']
   , consistency_level  = 0
+  , server_cache       = []
 ;
 
-// Server-side API
-exports.get = function(key){
-  console.log('server_get('+key+')');
-};
+io.sockets.on('connection', function (socket) {
+  console.log('connection established for WebSocket with wafer');
 
-exports.put = function(key, value){
-  console.log('server_put('+key+', '+value+')');
-};
+  // Just a test here
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  /**
+    * GET
+    *
+    */
+  socket.on(function(request){
+    console.log('server_get('+request.key+')');
+
+    socket.emit('get_ack');
+  };
+
+  /**
+    * PUT
+    *
+    */
+  socket.on(function(request){
+    console.log('server_put('+request.key+', '+request.value+')');
+
+    socket.emit('put_ack');
+  };
+});
 
 exports.set_consistency_level = function(new_consistency_level){
   consistency_level = new_consistency_level;
